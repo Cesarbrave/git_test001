5ee385f1f4551be33e8e4da77be0cd819db56527
Layne <Layne@atguigu.com>
2021-08-15 11:57:14 +0800

    my second commit by idea

--- src/main/java/com/atguigu/git/Gittest001.java
+++ src/main/java/com/atguigu/git/Gittest001.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Gittest001 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
